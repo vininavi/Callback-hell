1a44309031f7e679127ed510975fa3239bc82984
vinithra <vinissb23698@gmail.com>
2024-06-19 02:06:27 +0530

    upd

---
+++ js/script.js
@@ -0,0 +1,53 @@
+let timer=10;
+let time = document.getElementById('display');
+time.style.margin= "auto";
+time.style.textAlign = "center";
+time.style.maxWidth ="200px"
+setTimeout(() => {
+time.innerHTML = timer--;
+time.style.fontSize = "50px";
+setTimeout(() =>{
+time.className = "badge rounded-pill bg-light text-dark";
+time.innerHTML = timer--;
+setTimeout(() =>{
+time.className = "badge rounded-pill bg-danger";
+time.innerHTML = timer--;
+setTimeout(() =>{
+time.className = "badge rounded-pill bg-light text-dark";
+time.innerHTML = timer--;
+setTimeout(() =>{
+time.className = "badge rounded-pill bg-danger";
+time.innerHTML = timer--;
+setTimeout(() =>{
+time.className = "badge rounded-pill bg-light text-dark";
+time.innerHTML = timer--;
+setTimeout(() =>{
+time.className = "badge rounded-pill bg-danger";
+time.innerHTML = timer--;
+setTimeout(() =>{
+time.className = "badge rounded-pill bg-light text-dark";
+time.innerHTML = timer--;
+setTimeout(() =>{
+time.className = "badge rounded-pill bg-danger";
+time.innerHTML = timer--;
+setTimeout(() =>{
+time.className = "badge rounded-pill bg-light text-dark";
+time.innerHTML = timer--;
+setTimeout(() =>{
+time.style.fontSize = "50px";
+time.style.maxWidth = "700px";
+time.className = "badge rounded-pill bg-danger";
+time.innerHTML = "Happy Independence Day"
+                                                    
+},1000)
+},1000)
+},1000)
+},1000)
+},1000)
+},1000)
+},1000)
+},1000)     
+},1000)
+},1000)
+}, 1000); 
+    
